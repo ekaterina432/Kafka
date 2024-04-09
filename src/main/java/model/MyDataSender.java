c1015e7bf2704b103f4319936cf720db068a5915
KatePrish <prishchepa@sfedu.ru>
2024-04-09 13:52:26 +0300

    spring-boot start

--- src/main/java/model/MyDataSender.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface MyDataSender {
-    void send(StringValue value);
-
-}
--- src/main/java/my/ProducerApp.java
+++ src/main/java/my/ProducerApp.java
@@ -1,3 +1,5 @@
+package my;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
